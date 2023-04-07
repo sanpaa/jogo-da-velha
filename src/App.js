62d782e7c8eed17e7392a96fb7023543da518c94
sanpaa <paulosanches2002@live.com>
2023-04-07 19:52:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Just trying to learn react  src/app.js 
         </p>
         <a
           className="App-link"
